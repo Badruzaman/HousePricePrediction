a250bb4e0ff5107380740a4f15da7060333dd1ef
Badruzzaman <badru.cse@gmail.com>
2021-04-10 11:46:22 +0600

    New update

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
 
 USAhousing = pd.read_csv('USA_Housing.csv')
-#print(USAhousing)
-#USAhousing.head()
-#USAhousing.info()
-#USAhousing.describe()
 
-#USAhousing.columns
+X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+               'Avg. Area Number of Bedrooms', 'Area Population']]
+y = USAhousing['Price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+print(lm.intercept_)
+coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient'])
+print(coeff_df)
 
-#sns.pairplot(USAhousing)
+#boston = load_boston()
+#print(boston.DESCR)
+#boston_df = boston.data
 
-#sns.distplot(USAhousing['Price'])
-
-sns.heatmap(USAhousing.corr())
+predictions = lm.predict(X_test)
+print(predictions)
+plt.scatter(y_test,predictions)
